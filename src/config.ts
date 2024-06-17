1972368f4ba97b01b9609da71f7b628b838964fc
Gustav Grusell <gustav.grusell@eyevinn.se>
2024-06-17 08:22:44 +0200

    feat: first implementation
    Signed-off-by: Gustav Grusell <gustav.grusell@eyevinn.se>

---
+++ src/config.ts
@@ -0,0 +1,24 @@
+export interface Config {
+  watchFolder: string;
+  encoreParams: {
+    profile: string;
+    progressCallbackUri?: string;
+    inputFolder?: string;
+    outputFolder: string;
+    url: string;
+  };
+
+}
+
+export function readConfig(): Config {
+  return {
+    watchFolder: process.env.WATCH_FOLDER || 'watch',
+    encoreParams: {
+      profile: process.env.ENCORE_PROFILE || 'program',
+      progressCallbackUri: process.env.ENCORE_CALLBACK_URL,
+      inputFolder: process.env.ENCORE_INPUT_FOLDER,
+      outputFolder: process.env.ENCORE_OUTPUT_FOLDER || 'out',
+      url: process.env.ENCORE_URL || 'http://localhost:8080',
+    }
+  };
+}
---
+++ src/index.ts
@@ -0,0 +1,37 @@
+import { watch } from 'chokidar';
+import { EncoreUploadModule, EncoreUploadModuleOptions } from '@eyevinn/iaf-plugin-encore-local';
+import { Logger } from 'eyevinn-iaf';
+import { readConfig } from './config';
+import path from 'node:path'
+
+const logger: Logger = {
+  verbose: (message: string) => console.log(`[VERBOSE] ${message}`),
+  info: (message: string) => console.log(`[INFO] ${message}`),
+  warn: (message: string) => console.log(`[WARN] ${message}`),
+  error: (message: string) => console.error(`[ERROR] ${message}`)
+};
+
+const config = readConfig();
+
+const encoreUploader = new EncoreUploadModule({
+  encoreEndpoint: config.encoreParams.url,
+  inputLocation: config.encoreParams.inputFolder,
+  outputDestination: config.encoreParams.outputFolder,
+  encodeParams: config.encoreParams,
+  logger,
+  monitorJobs: false
+} as EncoreUploadModuleOptions);
+
+console.log(`Watching for new files in ${(config.watchFolder)}`);
+const watcher = watch([`${(config.watchFolder)}/**/*.mp4`], {
+  persistent: true,
+  ignoreInitial: true
+});
+
+watcher.on('add', (filePath) => {
+  const encorePath = config.encoreParams.inputFolder
+    ? filePath.replace(config.watchFolder, config.encoreParams.inputFolder)
+    : path.resolve(filePath);
+  console.log(`Will transcode '${path}'(${encorePath})`);
+  encoreUploader.onFileAdd(encorePath, undefined as any);
+});
